017f3aa48bad13808f552df32915b95a6ffd7850
19seniman <161638164+19seniman@users.noreply.github.com>
2025-06-14 02:07:06 +0700

    lim.js

--- lim.js
+++ lim.js
@@ -860,7 +860,7 @@ function safeRender() {
   setTimeout(() => {
     try {
       if (!isHeaderRendered) {
-        figlet.text("SHADOW ECONOMY", { font: "Rectangles" }, (err, data) => {
+        figlet.text("SHADOW ECONOMY", { font: "Isometric1" }, (err, data) => {
           if (!err) headerBox.setContent(`{center}{bold}{cyan-fg}${data}{/cyan-fg}{/bold}{/center}`);
           isHeaderRendered = true;
         });
