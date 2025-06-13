9f3ece836db16950dd4ea70be5c671b1262dd192
19seniman <161638164+19seniman@users.noreply.github.com>
2025-06-14 02:00:43 +0700

    lim.js

--- lim.js
+++ lim.js
@@ -860,7 +860,7 @@ function safeRender() {
   setTimeout(() => {
     try {
       if (!isHeaderRendered) {
-        figlet.text("SHADOW ECONOMY", { font: "Big Money-ne" }, (err, data) => {
+        figlet.text("SHADOW ECONOMY", { font: "Banner3-D" }, (err, data) => {
           if (!err) headerBox.setContent(`{center}{bold}{cyan-fg}${data}{/cyan-fg}{/bold}{/center}`);
           isHeaderRendered = true;
         });
